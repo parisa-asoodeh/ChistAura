9842188d33f2b91d8c580a9ac559838501eb5586
Parisa Asoodeh <parisaasoodeh70@gmail.com>
2026-08-21 12:17:23 +0430

    Start of UI/UX phase: Configuring the settings.py file

--- competition/competition/settings.py
+++ competition/competition/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
 LOGIN_URL = 'login'
