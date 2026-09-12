ce7f899ad1c279c474cd51db7bb049e2a48127ba
Parisa Asoodeh <parisaasoodeh70@gmail.com>
2026-09-12 11:51:35 +0430

    Limiting the display of matches on the match list page

--- competition/games/views/match_views.py
+++ competition/games/views/match_views.py
@@ -1,13 +1,35 @@
 from ..models import Match
 from django.shortcuts import render, get_object_or_404
-from games.models import MatchPlayerScore
 from games.match_detail_service import MatchDetailService
+from django.core.exceptions import PermissionDenied
+from teams.models import TeamMembership
+from django.db.models import Q
 
 
 
 def match_list(request):
 
     matches = Match.objects.all().order_by('-played_at')
+
+    if request.user.is_authenticated and not request.user.is_staff:
+
+        user_team = TeamMembership.objects.filter(
+            user=request.user
+        ).values_list(
+            'team_id',
+            flat=True
+        ).first()
+
+        if user_team:
+
+            matches = matches.filter(
+                Q(team1_id=user_team) |
+                Q(team2_id=user_team)
+            )
+
+        else:
+
+            matches = matches.none()
 
     return render(
         request,
@@ -25,6 +47,19 @@ def match_detail(request, match_id):
         Match,
         id=match_id,
     )
+
+    if request.user.is_authenticated and not request.user.is_staff:
+
+        is_member = TeamMembership.objects.filter(
+            user=request.user,
+            team__in=[
+                match.team1_id,
+                match.team2_id,
+            ],
+        ).exists()
+
+        if not is_member:
+            raise PermissionDenied
 
     context = MatchDetailService.build(
         match
